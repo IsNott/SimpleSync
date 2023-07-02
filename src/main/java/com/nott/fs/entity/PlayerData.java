fdf033c267128dff38ca6898318b9e217062a4a9
Zzzwlong <1448832395@qq.com>
2023-07-02 22:40:46 +0800

    feat:Deserialize ItemStack From Db by PlayData includes ItemMeta. add:ArmorItems field in PlayData.

--- src/main/java/com/nott/fs/entity/PlayerData.java
+++ src/main/java/com/nott/fs/entity/PlayerData.java
@@ -30,6 +30,8 @@ public class PlayerData {
 
     private int expToLevel;
 
+    private String armorItems;
+
     public PlayerData() {
 
     }
--- src/main/java/com/nott/fs/listener/EventListener.java
+++ src/main/java/com/nott/fs/listener/EventListener.java
@@ -74,59 +74,34 @@ public class EventListener implements Listener {
         int expToLevel = player.getExpToLevel();
         double health = player.getHealth();
         float exp = player.getExp();
-        String displayName = player.getDisplayName();
         String uuid = player.getUniqueId().toString();
         int level = player.getLevel();
         int foodLevel = player.getFoodLevel();
+
         PlayerInventory inventory = player.getInventory();
         ItemStack[] storageContents = inventory.getStorageContents();
+        ItemStack[] armorContents = inventory.getArmorContents();
+        ItemStack itemInMainHand = inventory.getItemInMainHand();
         JSONArray itemArrays = new JSONArray();
+        JSONArray armorArrays = new JSONArray();
+        // set playerData's storageContents
         for (ItemStack content : storageContents) {
            if(content != null){
-               plugin.getLogger().info("content :" + content.toString());
-               JSONObject itemJson = new JSONObject();
-               int amount = content.getAmount();
-               String name = content.getType().name();
-               boolean hasItemMeta = content.hasItemMeta();
-               if(hasItemMeta){
-                   JSONObject metaJson = new JSONObject();
-                   ItemMeta meta = content.getItemMeta();
-                   boolean hasDisplayName = meta.hasDisplayName();
-                   boolean hasEnchants = meta.hasEnchants();
-                   boolean hasLore = meta.hasLore();
-                   boolean unbreakable = meta.isUnbreakable();
-                   if(hasEnchants){
-                       Map<Enchantment, Integer> map = meta.getEnchants();
-                       JSONArray enchArray = new JSONArray();
-                       Set<Enchantment> keySet = map.keySet();
-                       keySet.forEach(key -> {
-                           JSONObject en = new JSONObject();
-                           en.put("level",map.get(key));
-                           en.put("enchantment",key);
-                           enchArray.add(en);
-                       });
-                       metaJson.put("enchants",enchArray);
-                   }
-                   if(hasDisplayName){
-                       metaJson.put("disPlayName",meta.getDisplayName());
-                   }
-                   if(hasLore){
-                       //todo addLore
-                   }
-                   if(!unbreakable){
-                       short durability = content.getDurability();
-                       itemJson.put("durability",String.valueOf(durability));
-                   }
-                   itemJson.put("meta",metaJson);
-               }
-
-               itemJson.put("amount",amount);
-               itemJson.put("name",name);
+               JSONObject itemJson = this.getItemJson(content,itemInMainHand);
                itemArrays.add(itemJson);
            }
         }
+        // set playerData's armorContent
+        for (ItemStack content : armorContents) {
+            if(content != null){
+                JSONObject itemJson = this.getItemJson(content,null);
+                armorArrays.add(itemJson);
+            }
+        }
+        String armorArrayJsonStr = JSON.toJSONString(armorArrays);
         String itemsMetaJsonStr = JSON.toJSONString(itemArrays);
         plugin.getLogger().info("itemsMetaJsonStr :"+ itemsMetaJsonStr);
+        plugin.getLogger().info("armorArrayJsonStr :"+ armorArrayJsonStr);
         // 更新玩家数据到数据库
         PlayerData playerData = playerDao.getPlayerDataByUUID(uuid);
         if (playerData != null) {
@@ -137,14 +112,64 @@ public class EventListener implements Listener {
             playerData.setLevel(level);
             playerData.setExpToLevel(expToLevel);
             playerData.setFoodLevel(foodLevel);
+            playerData.setArmorItems(armorArrayJsonStr);
             playerDao.updatePlayerById(playerData);
         }
+    }
+
+    private JSONObject getItemJson(ItemStack content,ItemStack itemInMainHand) {
+        plugin.getLogger().info("ItemStack :" + content.toString());
+        JSONObject itemJson = new JSONObject();
+        int amount = content.getAmount();
+        String name = content.getType().name();
+        boolean hasItemMeta = content.hasItemMeta();
+        if(hasItemMeta){
+            JSONObject metaJson = new JSONObject();
+            ItemMeta meta = content.getItemMeta();
+            plugin.getLogger().info("ItemMeta :" + meta.toString());
+            boolean hasDisplayName = meta.hasDisplayName();
+            boolean hasEnchants = meta.hasEnchants();
+            boolean hasLore = meta.hasLore();
+            boolean unbreakable = meta.isUnbreakable();
+            if(hasEnchants){
+                Map<Enchantment, Integer> map = meta.getEnchants();
+                JSONArray enchArray = new JSONArray();
+                Set<Enchantment> keySet = map.keySet();
+                keySet.forEach(key -> {
+                    JSONObject en = new JSONObject();
+                    en.put("level",map.get(key));
+                    en.put("enchantment",key);
+                    enchArray.add(en);
+                });
+                metaJson.put("enchants",enchArray);
+            }
+            if(hasDisplayName){
+                metaJson.put("displayName",meta.getDisplayName());
+            }
+            if(hasLore){
+                //todo addLore
+            }
+            if(!unbreakable){
+                short durability = content.getDurability();
+                itemJson.put("durability",String.valueOf(durability));
+            }
+            itemJson.put("meta",metaJson);
+        }
+        if(itemInMainHand != null){
+            if(content.equals(itemInMainHand)){
+                itemJson.put("inHand","Y");
+            }
+        }
+        itemJson.put("amount",amount);
+        itemJson.put("name",name);
+        return itemJson;
     }
 
     private void syncPlayerData(PlayerData playerData, Player player) {
         Double health = playerData.getHealth();
         Float exp = playerData.getExp();
         int foodLevel = playerData.getFoodLevel();
+        ItemStack itemInHand = null;
         // 同步玩家数据到游戏中
         player.setDisplayName(playerData.getDisplayName());
         if(health > 0.0D){
@@ -157,51 +182,107 @@ public class EventListener implements Listener {
             player.setFoodLevel(foodLevel);
         }
         player.setLevel(playerData.getLevel());
-        //todo sync PlayerInventory
+
         String items = playerData.getItems();
+        String armorItem = playerData.getArmorItems();
+        PlayerInventory inventory = player.getInventory();
+        // sync storageContents
         if(StringUtils.isNotEmpty(items)){
-            PlayerInventory inventory = player.getInventory();
             JSONArray itemsArray = JSONArray.parseArray(items);
             Iterator<Object> iterator = itemsArray.iterator();
-            ItemMeta itemMeta = null;
             ArrayList<ItemStack> itemStacks = new ArrayList<>();
             while (iterator.hasNext()){
                 JSONObject itemJson = (JSONObject) iterator.next();
-                boolean isUnBreakAble = itemJson.containsKey("durability") && StringUtils.isNotEmpty(itemJson.getString("durability"));
-                boolean hasMeta = itemJson.containsKey("meta");
-                Material material = Material.getMaterial(itemJson.getString("name"));
-                if(hasMeta){
-                    JSONObject meta = itemJson.getJSONObject("meta");
-                    itemMeta = Bukkit.getItemFactory().getItemMeta(material);
-                    itemMeta.setDisplayName(meta.containsKey("displayName")? meta.getString("displayName") : null);
-                    if(meta.containsKey("enchants")){
-                        JSONArray enchants = meta.getJSONArray("enchants");
-                        Iterator<Object> enchantIterator = enchants.iterator();
+                ItemStack itemStack = deserializeFromDb(itemJson);
 
-                        while (enchantIterator.hasNext()){
-                            JSONObject enchantment = (JSONObject) enchants.iterator();
-                            Enchantment en = Enchantment.getByName(enchantment.getString("name"));
-                            if(en != null){
-                                itemMeta.addEnchant(en,enchantment.getIntValue("level"),false);
-                            }
-                        }
-                    }
-                }
-                String amount = itemJson.getString("amount");
-                ItemStack itemStack = new ItemStack(material, Integer.parseUnsignedInt(amount));
-                if(!isUnBreakAble){
-                    String durability = itemJson.getString("durability");
-                    itemStack.setDurability(Short.parseShort(durability));
-                }
-                if(itemMeta != null && Bukkit.getItemFactory().isApplicable(itemMeta,itemStack)){
-                    itemStack.setItemMeta(itemMeta);
+                if(itemJson.containsKey("inHand") && "Y".equals(itemJson.getString("inHand"))){
+                    itemInHand = itemStack;
                 }
                 itemStacks.add(itemStack);
             }
             if(!itemStacks.isEmpty()){
                 inventory.setContents(itemStacks.toArray(new ItemStack[itemStacks.size()]));
             }
+
+            if(itemInHand != null){
+                inventory.setItemInMainHand(itemInHand);
+            }
+        }
+        // sync armorContents
+        if(StringUtils.isNotEmpty(armorItem)){
+            JSONArray itemsArray = JSONArray.parseArray(armorItem);
+            Iterator<Object> iterator = itemsArray.iterator();
+            ArrayList<ItemStack> armorItemStacks = new ArrayList<>();
+            while (iterator.hasNext()){
+                JSONObject itemJson = (JSONObject) iterator.next();
+                ItemStack itemStack = deserializeFromDb(itemJson);
+                armorItemStacks.add(itemStack);
+            }
+            if(!armorItemStacks.isEmpty()){
+                for (ItemStack armorItemStack : armorItemStacks) {
+                    String name = armorItemStack.getData().getItemType().name();
+                    if(name.contains("CHESTPLATE")){
+                        inventory.setChestplate(armorItemStack);
+                    }
+                    else if(name.contains("BOOT")){
+                        inventory.setBoots(armorItemStack);
+                    }
+                    else if(name.contains("LEGGINGS")){
+                        inventory.setLeggings(armorItemStack);
+                    } else {
+                        inventory.setHelmet(armorItemStack);
+                    }
+                }
+            }
+
         }
 
+    }
+
+    private ItemStack deserializeFromDb(JSONObject itemJson) {
+        boolean isUnBreakAble = itemJson.containsKey("durability") && StringUtils.isNotEmpty(itemJson.getString("durability"));
+        boolean hasMeta = itemJson.containsKey("meta");
+        ItemMeta itemMeta = null;
+        Material material = Material.getMaterial(itemJson.getString("name"));
+        if(hasMeta){
+            plugin.getLogger().info(String.format("current item [%s] has meta",material.name()));
+            JSONObject meta = itemJson.getJSONObject("meta");
+            itemMeta = Bukkit.getItemFactory().getItemMeta(material);
+            itemMeta.setDisplayName(meta.containsKey("displayName")? meta.getString("displayName") : null);
+            plugin.getLogger().info(String.format("displayName: %s",itemMeta.getDisplayName()));
+            if(meta.containsKey("enchants")){
+                JSONArray enchants = meta.getJSONArray("enchants");
+                Iterator<Object> enchantIterator = enchants.iterator();
+
+                while (enchantIterator.hasNext()){
+                    JSONObject enchant = (JSONObject) enchantIterator.next();
+                    JSONObject enchantment = enchant.getJSONObject("enchantment");
+                    Enchantment en = Enchantment.getByName(enchantment.getString("name"));
+                    if(en != null){
+                        plugin.getLogger().info(String.format("en: %s",en.toString()));
+                        itemMeta.addEnchant(en,enchant.getIntValue("level"),false);
+                    }
+                }
+            }
+            plugin.getLogger().info(String.format("current meta [%s]",itemMeta.toString()));
+        }
+        String amount = itemJson.getString("amount");
+        ItemStack itemStack = new ItemStack(material, Integer.parseUnsignedInt(amount));
+        if(!isUnBreakAble){
+            String durability = itemJson.getString("durability");
+            if(StringUtils.isNotEmpty(durability)){
+                itemStack.setDurability(Short.parseShort(durability));
+            }
+        }
+
+        if(itemMeta != null){
+            boolean applicable = Bukkit.getItemFactory().isApplicable(itemMeta, itemStack);
+            plugin.getLogger().info(String.format("current meta applicable %s",applicable));
+            boolean setItemMeta = itemStack.setItemMeta(applicable ? itemMeta : null);
+            if(!setItemMeta){
+                throw new RuntimeException("setItemMeta error");
+            }
+        }
+        return itemStack;
     }
 }
